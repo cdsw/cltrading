6fbff61c3a44a4c79378a04b87536b01d5004609
CL <cdswibowo@gmail.com>
2024-02-10 13:05:44 +0900

    disc agent

--- rlold/ENV.py
+++ rlold/ENV.py
@@ -1,11 +1,12 @@
-MAX_ACCOUNT_BALANCE = 2147483647
-MAX_NUM_UNITS = 100000
-MAX_UNIT_PRICE = 200000
 INITIAL_ACCOUNT_BALANCE = 10000
 WINDOW_SIZE = 5
-FEATURES = 5 # close, volume, delta, amplitude, close/low
-MDD_REWARD = 0.5
+FEATURES = ['close', 'volume', 'delta', 'amplitude', 'close_over_low']
+LEN_FEATURES = len(FEATURES)
 ACTIONS = ['long', 'short', 'hold', 'close']
 LEVERAGE = [1]
-TRADING_FEE_OPEN = 0
-TRADING_FEE_CLOSE = 0
+TRADING_FEE_OPEN = 0.0005 # portion
+TRADING_FEE_CLOSE = 0.0005 # portion
+MINIMUM_TRANSACTION_DOLLAR = 5
+PRECISION = 8 # precision for unit
+
+REWARD = {'open': 0, 'hold': 0, 'close': 0}
