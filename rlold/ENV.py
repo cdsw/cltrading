150885f2e19ed6f2ad30e66323a50f483200abb5
Damian Satya Wibowo (CL) <cdswibowo@gmail.com>
2024-03-18 22:54:09 +0900

    file flow chg, added sb3,-contrib, remodeled observn

--- rlold/ENV.py
+++ rlold/ENV.py
@@ -2,7 +2,8 @@ INITIAL_ACCOUNT_BALANCE = 100000
 WINDOW_SIZE = 1
 EMA = [8,28,88]
 
-FEATURES = ['close', 'volume', 'delta', 'amplitude', 'close_over_low', ]
+# FEATURES = ['close', 'volume', 'delta', 'amplitude', 'close_over_low', ]
+FEATURES = ['volume', 'delta', 'amplitude', 'close_over_low']
 for e in EMA:
   FEATURES.append(f'EMA_{e}')
 
@@ -15,4 +16,6 @@ MINIMUM_TRANSACTION_DOLLAR = 5
 PRECISION = 8 # precision for unit
 
 INTERVAL = 100000
-EPISODE = 5
+EPISODE = 4
+
+LOG_LOCATION = "./results/logs_"
--- rlold/Functions.py
+++ rlold/Functions.py
@@ -1,19 +1,52 @@
 import pandas as pd
 import pandas_ta as ta
 from ENV import *
+import matplotlib.pyplot as plt
+import numpy as np
 
-def add_ema(df, ma_widths=EMA):
-    res = [df]
-    for x in ma_widths:
-      alpha = 2 / (x + 1)  # EMA smoothing factor
-      ema_values = [df['close'].iloc[0]]  # Start with the first value as the initial EMA
+def getStationary(df, fn, scaling=True):
+  df['delta'] = (df['close'] / df ['open']) - 1
+  df['amplitude'] = (df['high'] / df['low']) - 1
+  df['close_over_low'] = (df['close'] / df ['low']) - 1
+  del df['open']
+  del df['high']
+  del df['low']
+  df['volume'].replace(0, 0.00001, inplace=True)
 
-      for i in range(1, len(df['close'])):
-        ema = alpha * df['close'].iloc[i] + (1 - alpha) * ema_values[-1]
-        ema_values.append(ema)
+  # Scaling
+  if scaling:
+    df = scale(df, ['delta','amplitude','close_over_low','volume'])
 
-      df[f'EMA_{x}'] = ema_values
-    return df
+  # Saving
+  df.to_csv(f'../data/{fn}-full.csv')
+  df.iloc[:500].to_csv(f'../data/{fn}-short.csv')
+  df.iloc[:10000].to_csv(f'../data/{fn}-long.csv')
+  return df
+
+def scale(df, column_list): #-1 to 1
+  for x in column_list:
+    scaled = np.log(np.sqrt(df[x] - df[x].min() + 1))
+    max_value = scaled.max()
+    min_value = scaled.min()
+    scaled = 2 * ((scaled - min_value) / (max_value - min_value)) - 1 # -1 to 1
+    df[x] = scaled
+  return df
+
+def add_ema(df, normalize=False, ma_widths=EMA):
+  res = df
+  for x in ma_widths:
+    alpha = 2 / (x + 1)  # EMA smoothing factor
+    ema_values = [df['close'].iloc[0]]  # Start with the first value as the initial EMA
+
+    for i in range(1, len(df['close'])):
+      ema = alpha * df['close'].iloc[i] + (1 - alpha) * ema_values[-1]
+      ema_values.append(ema)
+
+    res[f'EMA_{x}'] = ema_values
+    if normalize:
+      res[f'EMA_{x}'] = res[f'EMA_{x}'] / res['close']
+  return res
+
 
 
 # df = pd.DataFrame({
