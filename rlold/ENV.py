71f116e4cad92730ac1e04f917ba5f18bdf69445
Damian Satya Wibowo (CL) <cdswibowo@gmail.com>
2024-04-09 00:56:14 +0900

    Lev fix

--- rlold/ENV.py
+++ rlold/ENV.py
@@ -1,5 +1,5 @@
 INITIAL_ACCOUNT_BALANCE = 100000
-WINDOW_SIZE = 5
+WINDOW_SIZE = 4
 EMA = [8,28,88]
 
 # FEATURES = ['close', 'volume', 'delta', 'amplitude', 'close_over_low', ]
@@ -10,13 +10,13 @@ ADDITIONAL_FEATURES = ['benchmark_movement', 'profit_movement']
 
 LEN_FEATURES = len(FEATURES)
 ACTIONS = ['long', 'short', 'hold', 'close']
-LEVERAGE = 5 # maximum leverage
+LEVERAGE = 4 # maximum leverage
 TRADING_FEE_OPEN = 0.0005 # portion
 TRADING_FEE_CLOSE = 0.0005 # portion
 MINIMUM_TRANSACTION_DOLLAR = 5
 PRECISION = 8 # precision for unit
 
 INTERVAL = 100000
-EPISODE = 1
+EPISODE = 6
 
 LOG_LOCATION = "./results/logs_"
