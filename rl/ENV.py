eeba3d12b3ceba738669954185790ae9f0de1fb7
Damian Satya Wibowo (CL) <cdswibowo@gmail.com>
2024-04-14 19:40:21 +0900

    restructure

---
+++ rl/ENV.py
@@ -0,0 +1,22 @@
+INITIAL_ACCOUNT_BALANCE = 100000
+WINDOW_SIZE = 5
+EMA = [8, 28, 88]
+
+# FEATURES = ['close', 'volume', 'delta', 'amplitude', 'close_over_low', ]
+FEATURES = ['volume', 'delta', 'amplitude', 'close_over_low']
+for e in EMA:
+  FEATURES.append(f'EMA_{e}')
+ADDITIONAL_FEATURES = ['benchmark_movement', 'profit_movement']
+
+LEN_FEATURES = len(FEATURES)
+ACTIONS = ['long', 'short', 'hold', 'close']
+LEVERAGE = 1 # maximum leverage
+TRADING_FEE_OPEN = 0.0005 # portion
+TRADING_FEE_CLOSE = 0.0005 # portion
+MINIMUM_TRANSACTION_DOLLAR = 5
+PRECISION = 8 # precision for unit
+
+INTERVAL = 100000
+EPISODE = 4
+
+LOG_LOCATION = "./results/logs_"
--- xclear_log.bat
+++ xclear_log.bat
@@ -1,2 +1,2 @@
-del .\rlold\results\logs_\* /S /Q
-FOR /D %%p IN (".\rlold\results\logs_\*") DO rmdir "%%p" /s /q
+del .\rl\results\logs_\* /S /Q
+FOR /D %%p IN (".\rl\results\logs_\*") DO rmdir "%%p" /s /q
